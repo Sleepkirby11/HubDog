795c65c6120cfa205299f6403472a9238585fe41
Sleepkirby11 <69339024+Sleepkirby11@users.noreply.github.com>
2026-04-09 23:36:03 +0900

    2026.04.09 (0.0.10v)
    =스크립트 폴더 정리
    =Ting의 OnColliderStay => Enter로 변경

--- Assets/Scripts/PlayerController/Ting.cs
+++ Assets/Scripts/PlayerController/Ting.cs
@@ -22,7 +22,7 @@ public class Ting : MonoBehaviour
         
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("EnemyBullet") && GameManager.instance.player.isParrying)
         {
